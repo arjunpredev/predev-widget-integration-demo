2ae12bce446de056bcddcfea3604906514fe64fa
Adam Elkassas <adamelk16@gmail.com>
2023-09-24 22:49:36 -0400

    remove particles

--- src/components/Landing/HeroSection.jsx
+++ src/components/Landing/HeroSection.jsx
@@ -1,84 +1,10 @@
 import { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-
-const particleOptions = {
-	particles: {
-		number: {
-			value: 80,
-			density: {
-				enable: true,
-				value_area: 800,
-			},
-		},
-		color: {
-			value: ["#ffffff", "#ff9900", "#00ff00"],
-		},
-
-		shape: {
-			type: "circle",
-		},
-		opacity: {
-			value: 0.5,
-			random: false,
-			animation: {
-				enable: true,
-				speed: 1,
-				minimumValue: 0.1,
-				sync: false,
-			},
-		},
-		size: {
-			value: 3,
-			random: true,
-			animation: {
-				enable: false,
-				speed: 20,
-				minimumValue: 0.1,
-				sync: false,
-			},
-		},
-		lineLinked: {
-			enable: true,
-			distance: 150,
-			color: "#ffffff",
-			opacity: 0.4,
-			width: 1,
-		},
-		move: {
-			enable: true,
-			speed: 0.5,
-			direction: "none",
-			random: false,
-			straight: false,
-			outModes: {
-				default: "out",
-			},
-			attract: {
-				enable: false,
-				rotateX: 600,
-				rotateY: 1200,
-			},
-		},
-	},
-	detectRetina: true,
-};
 
 export const HeroSection = ({ onGetStarted }) => {
 	const [isTablet, setIsTablet] = useState(window.innerHeight > 1000);
 
 	const navigate = useNavigate();
-	const particlesInit = useCallback(async (engine) => {
-		console.log(engine);
-		// you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-		// starting from v2 you can add only the features you need reducing the bundle size
-		await loadFull(engine);
-	}, []);
-	const particlesLoaded = useCallback(async (container) => {
-		await console.log(container);
-	}, []);
 	return (
 		<div className="hero-section">
 		
