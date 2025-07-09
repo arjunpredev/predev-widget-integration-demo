bf24515c7010cc32409e72940b1b7ec8adc03628
eip_achilles <arjun@pre.dev>
2025-07-09 19:41:30 -0400

    Update LandingPage to use dynamic API key for iframe source, enhancing flexibility and integration with environment variables.

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -5,7 +5,7 @@ export const LandingPage = () => {
 		<>
 			<main className="hero-section min-h-screen flex justify-center items-center">
 				<iframe
-					src="https://pre.dev/iframe/enterprise/hero/6e4b9523-bb22-4762-b1fd-1e414f1c27f2"
+					src={`https://pre.dev/iframe/enterprise/hero/${process.env.REACT_APP_PREDEV_API_KEY}`}
 					style={{
 						width: "100%",
 						height: "100vh",
